11aa4cf53a3330093b827d60bb948d222e8a97e7
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-15 19:46:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,16 +13,18 @@ const codes = [
 
 function init() {
   index = 0;
-  document.body.addEventListener('keydown', function(e) {
-    const key = e.key;
-    if(key == codes[index]) {
-      index++;
-      if(index === codes.length) {
-        window.alert("infinte lives");
-        index = 0;
-      }
-    } else {
+  document.body.addEventListener('keydown', wert);
+}
+
+function wert(e) {
+  const key = e.key;
+  if(key == codes[index]) {
+    index++;
+    if(index === codes.length) {
+      window.alert("infinte lives");
       index = 0;
     }
-  });
+  } else {
+    index = 0;
+  }
 }
