e7c1e1c6665465a77fb502f371b932e77a14b31b
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-15 19:41:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,7 @@ const codes = [
 
 function init() {
   let index = 0;
-  document.body.addEventListener('keydown', wert(event));
+  document.body.addEventListener('keydown', wert(e));
 }
 
 function wert(e) {
